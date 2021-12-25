7be988609c571708b1a3951281f597aa9db0d022
LoveAndCode <practice1356@gmail.com>
2021-12-26 00:14:57 +0900

    project init

---
+++ src/main/java/com/tistory/johnmark/springmvc/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.tistory.johnmark.springmvc;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringMVC1Application.class);
+	}
+
+}
---
+++ src/test/java/com/tistory/johnmark/springmvc/SpringMVC1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tistory.johnmark.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMVC1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
